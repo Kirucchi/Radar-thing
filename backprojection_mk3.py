ec2f260d532b2df33bd6f3bd0583abb7922d9a56
Kiryu Sakakibara <kiryusaka@gmail.com>
2018-07-23 15:28:07 -0400

    Changed everything into variables

--- backprojection_mk3.py
+++ backprojection_mk3.py
@@ -29,20 +29,23 @@ Unfinished
 def edge_detection(arr):
     return
 
-radar_positions = data[0]
-pulses = data[1]
-range_bins = data[2][0]
-
 '''
 Stores the positions of the UAS
 '''
 radar_x = []
-for position in radar_positions:
+radar_y = []
+radar_z = []
+for position in data[0]:
     radar_x.append(position[0])
-radar_y = radar_positions[0][1]
-radar_z = radar_positions[0][2]
+    radar_y.append(position[1])
+    radar_z.append(position[2])
 
 '''
+pulses = data[1]
+range_bins = data[2][0]
+range_bin_d = (range_bins[int(len(range_bins)/2)+1] - range_bins[int(len(range_bins)/2)])/2
+...
+
 Initializes grid as a list of lists
 '''
 pixel_values = list(list(0+0j for ii in np.arange(0, size)) for jj in np.arange(0, size))
@@ -53,17 +56,18 @@ the correct range bins and ultimately storing the absolute value of the sum at t
 '''
 y = 2.5
 for ii in np.arange(0, size):
+    print ("%d / %d" % (ii, size))
     x = -2.5
     for jj in np.arange(0, size):
         for kk in np.arange(0, 100):
-            distance = get_range(radar_x[kk], x, y)
-            ratio = (distance % 0.0185) / 0.0185
-            index = math.floor(distance/0.0185)
+            distance = get_range(radar_x[kk], radar_y[kk], radar_z[kk], x, y)
+            ratio = (distance % range_bin_d) / range_bin_d
+            index = math.floor(distance/range_bin_d)
             pixel_values[ii][jj] += (pulses[kk][index]*(1-ratio) + pulses[kk][index+1]*(ratio))
         pixel_values[ii][jj] = np.abs(pixel_values[ii][jj])
         x = x + 5.0/size
     y = y - 5.0/size
-            
+          
 plt.imshow(pixel_values) # shows the image
 
 end = timeit.default_timer()

72a4d17c3d9755d1d821cbf52ee272f9267cea17
4llenw4ng <33169511+4llenw4ng@users.noreply.github.com>
2018-07-25 10:36:33 -0400

    Standardized almost everything as functions, now the program has to run with arg = number of pixels

--- backprojection_mk3.py
+++ backprojection_mk3.py
@@ -12,22 +12,13 @@ import timeit
 import argparse
 import sys
 import os
-from pulson440_constants import SPEED_OF_LIGHT
-
-'''
-start = timeit.default_timer()
-
-f = open(r"mandrill_no_aliasing_data.pkl", "rb")
-data = pickle.load(f)
-
-size = 500 # number of pixels per axis of the image, change to alter resolution
+#from pulson440_constants import SPEED_OF_LIGHT
 
 
 # Returns the distance(range) between the radar and a specific location in the 3D cartesian coordinate plane
 
-def get_range(radar_xpos, radar_ypos, radar_zpos, img_x, img_y, img_z):
-    return math.sqrt((radar_xpos - img_x)**2 + (radar_ypos - img_y)**2 + (radar_zpos - img_z)**2)
-'''
+def get_range(radar_xpos, radar_ypos, radar_zpos, img_x, img_y):
+    return math.sqrt((radar_xpos - img_x)**2 + (radar_ypos - img_y)**2 + (radar_zpos)**2)
 '''
 Plots a picture that illustrates the edges of the source image
 @param   arr   the 2D array or list of lists that contains the sum magnitude of pulses
@@ -54,8 +45,8 @@ must be less than size, and start x/y must be greater or equal to zero.
 def partImage returns a 2d array of magnitudes from a range of pixels defined
 by the parameters
 '''
-'''
-def partImage(start_x, start_y, end_x, end_y):
+
+def partImage(start_x, start_y, end_x, end_y, pulses, range_bin_d, radar_x, radar_y, radar_z, size):
     xDiff = np.abs(end_x - start_x)
     yDiff = (end_y - start_y)
     pulseArr = list(list(0+0j for ii in np.arange(0, xDiff)) for jj in np.arange(0, yDiff))
@@ -73,29 +64,8 @@ def partImage(start_x, start_y, end_x, end_y):
             x = x + 5.0/size
         y = y - 5.0/size
     return magArr
-'''
-'''
-returns entropic value of image. parameter is an array of magnitudes
-'''
-def get_entropy(magnitude_array):
-    entropy_sum = 0
-    minMag = -1 
-    maxMag = magnitude_array[0][0]
-    for yy in range(len(magnitude_array)):
-        for xx in range(len(magnitude_array[yy])):
-            curr_mag = magnitude_array[yy][xx]
-            if curr_mag > maxMag:
-                maxMag = curr_mag
-            if curr_mag < minMag:
-                minMag = curr_mag
-    
-    magDiff = np.abs(maxMag - minMag)
-    for yy in range(len(magnitude_array)):
-        for xx in range(len(magnitude_array[yy])):
-            curr_mag_final = np.abs(magnitude_array[yy][xx] - minMag)/magDiff
-            entropy_sum += curr_mag_final*np.log2(curr_mag_final)
-    return entropy_sum
 
+'''
 def fourier_approach(pulses, range_axis, platform_pos, x_vec, y_vec, 
                      center_freq):
     """
@@ -158,42 +128,38 @@ def fourier_approach(pulses, range_axis, platform_pos, x_vec, y_vec,
         # Update complex image with latest column's result
         complex_image[:, ii] = sum_aligned_pulses
     return complex_image
-
+'''
 def parse_args(args):
     """
     Input argument parser.
     """
     parser = argparse.ArgumentParser(
             description=('SAR image formation via backprojection'))
-    parser.add_argument('input', nargs='?', type=str,
-                        help='Pickle containing data')
-    parser.add_argument('x_bounds', nargs=2, type=float, 
-                        help=('Minimum and maximum bounds of the X coordinates'
-                              ' of the image (m)'))
-    parser.add_argument('y_bounds', nargs=2, type=float, 
-                        help=('Minimum and maximum bounds of the Y coordinates'
-                              ' of the image (m)'))
+    #parser.add_argument('input', nargs='?', type=str,
+    #                    help='Pickle containing data')
+    #parser.add_argument('x_bounds', nargs=2, type=float, 
+    #                    help=('Minimum and maximum bounds of the X coordinates'
+    #                          ' of the image (m)'))
+    #parser.add_argument('y_bounds', nargs=2, type=float, 
+    #                    help=('Minimum and maximum bounds of the Y coordinates'
+    #                          ' of the image (m)'))
     parser.add_argument('pixel_res', type=float, help='Pixel resolution (m)')
-    parser.add_argument('-o', '--output', nargs='?', const=None, default=None, 
-                        type=str, help='File to store SAR image to')
-    parser.add_argument('-m', '--method', nargs='?', type=str,
-                        choices=('shift', 'interp', 'fourier'),
-                        default='fourier', const='fourier', 
-                        help='Backprojection method to use')
-    parser.add_argument('-fc', '--center_freq', type=float, 
-                        help=('Center frequency (Hz) of radar; must be '
-                              'specified if using fourier method'))
-    parser.add_argument('-nv', '--no_visualize', action='store_true', 
-                        help='Do not show SAR image')
+    #parser.add_argument('-o', '--output', nargs='?', const=None, default=None, 
+    #                    type=str, help='File to store SAR image to')
+    #parser.add_argument('-m', '--method', nargs='?', type=str,
+    #                    choices=('shift', 'interp', 'fourier'),
+    #                    default='fourier', const='fourier', 
+    #                    help='Backprojection method to use')
+    #parser.add_argument('-fc', '--center_freq', type=float, 
+    #                    help=('Center frequency (Hz) of radar; must be '
+    #                          'specified if using fourier method'))
+    #parser.add_argument('-nv', '--no_visualize', action='store_true', 
+    #                    help='Do not show SAR image')
     parsed_args = parser.parse_args(args)
-    
     # Do some additional checks
-    if parsed_args.output is None:
-        root, ext = os.path.splitext(parsed_args.input)
-        parsed_args.output = '%s.png' % root
     
     return parsed_args
-
+'''
 def main(args):
     """
     Top level methods
@@ -226,6 +192,7 @@ def main(args):
     image = np.abs(complex_image)
         
     # Show SAR image
+    
     if not parsed_args.no_visualize:
         image_extent = (x_vec[0], x_vec[-1], y_vec[0], y_vec[-1])
         plt.figure()
@@ -238,7 +205,7 @@ def main(args):
         plt.title('Logarithmic Scale')
         plt.colorbar()
         plt.show()
-        
+    plt.imshow(edge_detection(image,1500))
     # Save image
     plt.imsave(parsed_args.output, image)
 
@@ -248,37 +215,51 @@ if __name__ == "__main__":
     """
     main(sys.argv[1:])
 '''
-#Stores the positions of the UAS
-radar_x = []
-radar_y = []
-radar_z = []
-for position in data[0]:
-    radar_x.append(position[0])
-    radar_y.append(position[1])
-    radar_z.append(position[2])
-pulses = data[1]
-range_bins = data[2][0]
-range_bin_d = (range_bins[int(len(range_bins)/2)+1] - range_bins[int(len(range_bins)/2)])/2
-# Initializes grid as a list of lists
-pixel_values = list(list(0+0j for ii in np.arange(0, size)) for jj in np.arange(0, size))
+    
+
 # Calculating the color of each pixel in the image by iterating over all the pulses and summing the complex values within
 # the correct range bins and ultimately storing the absolute value of the sum at the corresponding index in pixel_values 
-y = 2.5
-for ii in np.arange(0, size):
-    print ("%d / %d" % (ii, size))
-    x = -2.5
-    for jj in np.arange(0, size):
-        for kk in np.arange(0, 100):
-            distance = get_range(radar_x[kk], radar_y[kk], radar_z[kk], x, y)
-            ratio = (distance % range_bin_d) / range_bin_d
-            index = math.floor(distance/range_bin_d)
-            pixel_values[ii][jj] += (pulses[kk][index]*(1-ratio) + pulses[kk][index+1]*(ratio))
-        pixel_values[ii][jj] = np.abs(pixel_values[ii][jj])
-        x = x + 5.0/size
-    y = y - 5.0/size
-          
-plt.imshow(pixel_values) # shows the image
-#plt.imshow(edge_detection(pixel_values,1500)) # prints the image after edge detection
-end = timeit.default_timer()
-print(end-start) # prints the run time
-'''
+def linear_interp(pulses,range_bin_d,radar_x,radar_y,radar_z,size):
+     # Initializes grid as a list of lists
+    pixel_values = list(list(0+0j for ii in np.arange(0, size)) for jj in np.arange(0, size))
+    y = 2.5
+    for ii in np.arange(0, size):
+        print ("%d / %d" % (ii, size))
+        x = -2.5
+        for jj in np.arange(0, size):
+            for kk in np.arange(0, 100):
+                distance = get_range(radar_x[kk], radar_y[kk], radar_z[kk], x, y)
+                ratio = (distance % range_bin_d) / range_bin_d
+                index = math.floor(distance/range_bin_d)
+                pixel_values[ii][jj] += (pulses[kk][index]*(1-ratio) + pulses[kk][index+1]*(ratio))
+            pixel_values[ii][jj] = np.abs(pixel_values[ii][jj])
+            x = x + 5.0/size
+        y = y - 5.0/size
+    return pixel_values
+
+def main(args):
+    start = timeit.default_timer()
+    
+    f = open(r"mandrill_no_aliasing_data.pkl", "rb")
+    data = pickle.load(f)
+    size = (int)(args) # number of pixels per axis of the image, change to alter resolution
+    
+    #Stores the positions of the UAS
+    radar_x = []
+    radar_y = []
+    radar_z = []
+    for position in data[0]:
+        radar_x.append(position[0])
+        radar_y.append(position[1])
+        radar_z.append(position[2])
+    pulses = data[1]
+    range_bins = data[2][0]
+    range_bin_d = (range_bins[int(len(range_bins)/2)+1] - range_bins[int(len(range_bins)/2)])/2   
+    img = np.reshape(linear_interp(pulses,range_bin_d,radar_x,radar_y,radar_z,size),(size,size))
+    plt.imshow(img) # shows the image
+    plt.show()
+    #plt.imshow(edge_detection(img,1500)) # prints the image after edge detection
+    end = timeit.default_timer()
+    print(end-start) # prints the run time
+    
+main(sys.argv[1])
